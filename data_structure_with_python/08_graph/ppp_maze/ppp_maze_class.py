81b30c17345c5fb5628112260c13e27816c7c70f
kimdohwan <dosio0102@gmail.com>
2018-12-22 20:59:30 +0900

    <set_start_end()> 함수 완성, <hor, ver> 를 만져서 지점새성하던 코드를 <maze> 문자열에서 바로 생성하게 바꿈

--- data_structure_with_python/08_graph/ppp_maze/ppp_maze_class.py
+++ data_structure_with_python/08_graph/ppp_maze/ppp_maze_class.py
@@ -16,7 +16,6 @@ class Maze:
     def _make_maze(self):
 
         start_x, start_y = randrange(self.w), randrange(self.h)
-        # self._create_map(start_x, start_y, vis=vis, ver=ver, hor=hor)
         self._create_map(start_x, start_y)
 
         maze = ''
@@ -26,7 +25,6 @@ class Maze:
 
     # _make_maze() 에서 사용되는 재귀함수
     def _create_map(self, x, y):
-        # kwargs['vis'][y][x] = 1
         self.vis[y][x] = 1
 
         direction = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
@@ -41,12 +39,35 @@ class Maze:
 
             self._create_map(xx, yy)
 
-    def set_start_end(self):
-        randrange(len())
-        pass
+
 
     def solve(self):
         pass
+
+    def change_maze(self, arr, x, y, to_string):
+        arr[y] = arr[y][x:] + to_string + arr[y][x + 1:]
+        pass
+
+    # 출발, 도착 지점을 self.maze 에 만듭니다. random_point=True 라면 랜덤으로 지점 생성합니다.
+    def set_start_end(self, random_point=False):
+        if random_point:
+            while True:
+                start_point = randrange(len(self.maze))
+                end_point = randrange(len(self.maze))
+                if start_point != end_point and ' ' in self.maze[start_point] and ' ' in self.maze[end_point]:
+                    break
+        else:
+            for i, s in enumerate(self.maze):
+                if s == ' ':
+                    start_point = i
+                    break
+            for i, s in enumerate(reversed(self.maze)):
+                if s == ' ':
+                    end_point = len(self.maze) - i - 1
+                    break
+
+        self.maze = self.maze[:start_point] + 'S' + self.maze[start_point + 1:]
+        self.maze = self.maze[:end_point] + 'E' + self.maze[end_point + 1:]
 
     def __repr__(self):
         return f'Maze({self.w}, {self.h})'
@@ -59,4 +80,9 @@ if __name__ == '__main__':
     m1 = Maze(12, 6)
     print(repr(m1))
     print(m1)
-    # print(m1.solve())
+
+    m1.set_start_end(
+        # random_point=True
+    )
+
+    print(m1.maze)
