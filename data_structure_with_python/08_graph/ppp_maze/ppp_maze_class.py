36c08e511863bf04b57848698203e2579a518b90
kimdohwan <dosio0102@gmail.com>
2018-12-22 19:47:41 +0900

    make <vis, hor, ver> instance attribute(self), remove **kwargs

--- data_structure_with_python/08_graph/ppp_maze/ppp_maze_class.py
+++ data_structure_with_python/08_graph/ppp_maze/ppp_maze_class.py
@@ -5,37 +5,48 @@ class Maze:
     def __init__(self, width, height):
         self.w = width
         self.h = height
+
+        self.vis = [[0] * self.w + [1] for _ in range(self.h)] + [[1] * (self.w + 1)]
+        self.hor = [["##"] * self.w + ['#'] for _ in range(self.h + 1)]
+        self.ver = [["# "] * self.w + ['#'] for _ in range(self.h)] + [[]]
+
         self.maze = self._make_maze()
 
     # 미로를 생성 return type = str
     def _make_maze(self):
-        vis = [[0] * self.w + [1] for _ in range(self.h)] + [[1] * (self.w + 1)]
-        hor = [["##"] * self.w + ['#'] for _ in range(self.h + 1)]
-        ver = [["# "] * self.w + ['#'] for _ in range(self.h)] + [[]]
 
         start_x, start_y = randrange(self.w), randrange(self.h)
-        self._create_map(start_x, start_y, vis=vis, ver=ver, hor=hor)
+        # self._create_map(start_x, start_y, vis=vis, ver=ver, hor=hor)
+        self._create_map(start_x, start_y)
 
         maze = ''
-        for (h, v) in zip(hor, ver):
+        for (h, v) in zip(self.hor, self.ver):
             maze += ''.join(h + ['\n'] + v + ['\n'])
         return maze
 
     # _make_maze() 에서 사용되는 재귀함수
-    def _create_map(self, x, y, **kwargs):
-        kwargs['vis'][y][x] = 1
+    def _create_map(self, x, y):
+        # kwargs['vis'][y][x] = 1
+        self.vis[y][x] = 1
 
         direction = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
         shuffle(direction)
         for (xx, yy) in direction:
-            if kwargs['vis'][yy][xx]:
+            if self.vis[yy][xx]:
                 continue
             if xx == x:
-                kwargs['hor'][max(y, yy)][x] = '# '
+                self.hor[max(y, yy)][x] = '# '
             if yy == y:
-                kwargs['ver'][y][max(x, xx)] = '  '
+                self.ver[y][max(x, xx)] = '  '
 
-            self._create_map(xx, yy, **kwargs)
+            self._create_map(xx, yy)
+
+    def set_start_end(self):
+        randrange(len())
+        pass
+
+    def solve(self):
+        pass
 
     def __repr__(self):
         return f'Maze({self.w}, {self.h})'
@@ -48,3 +59,4 @@ if __name__ == '__main__':
     m1 = Maze(12, 6)
     print(repr(m1))
     print(m1)
+    # print(m1.solve())
