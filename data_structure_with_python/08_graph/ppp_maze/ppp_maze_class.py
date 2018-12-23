908e0ddb4df6e2ff8e908593e6c3bdd50cc512fe
kimdohwan <dosio0102@gmail.com>
2018-12-23 19:16:56 +0900

    property, getter, setter 수정 전

--- data_structure_with_python/08_graph/ppp_maze/ppp_maze_class.py
+++ data_structure_with_python/08_graph/ppp_maze/ppp_maze_class.py
@@ -1,7 +1,7 @@
 import sys
 from random import randrange, shuffle, seed
 
-seed(2)
+
 class Maze:
     def __init__(self, width, height):
         self.w = width
@@ -11,10 +11,34 @@ class Maze:
         self.hor = [["##"] * self.w + ['#'] for _ in range(self.h + 1)]
         self.ver = [["# "] * self.w + ['#'] for _ in range(self.h)] + [[]]
 
-        self.maze = self._make_maze()
+        self.convert_maze = None
+        self.distance = None
+        self.prev = None
+        self.visited = None
+
+        self.maze = self._maze
+
+    # 미로의 노드(길 1칸을 의미) 갯수. node size - 1 은 edge 의 최댓값이 된다.
+    @property
+    def node_size(self):
+        n = 0
+        for i in self._maze:
+            for j in i:
+                if j in [' ']:
+                    n += 1
+        return n
+
+    @node_size.setter
+    def node_size(self, value):
+        node_size = value
+
+    # @node_size.getter
+    # def node_size(self):
+    #     return 'getter'
 
     # 미로를 생성 return type = str
-    def _make_maze(self):
+    @property
+    def _maze(self):
 
         start_x, start_y = randrange(self.w), randrange(self.h)
         self._create_map(start_x, start_y)
@@ -24,7 +48,7 @@ class Maze:
             maze += ''.join(h + ['\n'] + v + ['\n'])
         return maze
 
-    # _make_maze() 에서 사용되는 재귀함수
+    # _maze() 에서 사용되는 재귀함수
     def _create_map(self, x, y):
         self.vis[y][x] = 1
 
@@ -41,68 +65,69 @@ class Maze:
             self._create_map(xx, yy)
 
     def solve(self):
-        convert_maze = self.maze.split('\n')
-        (sx, sy), (ex, ey) = self.get_start_end_point(convert_maze)
+        self.set_start_end_point()
+        self.convert_maze = self.maze.split('\n')
+        (sx, sy), (ex, ey) = self._get_start_end_point()
 
-        self._find_path(convert_maze, sx, sy, ex, ey)
-
-    def _find_path(self, convert_maze, sx, sy, ex, ey):
-        visited = self.copy_multidimensional_list(
-            li=convert_maze,
+        self.visited = self._copy_multidimensional_list(
+            li=self.convert_maze,
             data=False,
             target='#',
             target_data=None,
         )
 
-        distance = self.copy_multidimensional_list(
-            li=convert_maze,
-            data=(self.w * 2 - 1) * self.h,
+        self.distance = self._copy_multidimensional_list(
+            li=self.convert_maze,
+            data=self.node_size,
             target='#',
             # target_data=sys.maxsize,
-            target_data=99,
+            target_data=999,
         )
-        distance[sy][sx] = 0
+        self.distance[sy][sx] = 0
 
-        prev = self.copy_multidimensional_list(
-            li=convert_maze,
+        self.prev = self._copy_multidimensional_list(
+            li=self.convert_maze,
             data=None,
             target='#',
             target_data=[-1, -1],
         )
-        prev[sy][sx] = 1
+        self.prev[sy][sx] = 1
 
-        x, y = sx, sy
-        px, py = sx, sy
+        self._find_path(sx, sy)
+        self._get_minimum_path(sx, sy, ex, ey)
+
+        solve_maze = ''
+        for i in self.convert_maze:
+            solve_maze += ''.join(i + '\n')
+
+        return solve_maze
+
+    def _find_path(self, x, y):
         direction = [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1)]
-        stack = [] + direction
+        for xx, yy in direction:
+            if self.visited[yy][xx] is not None and self.distance[yy][xx] > self.distance[y][x] + 1:
+                self.distance[yy][xx] = self.distance[y][x] + 1
+                self.prev[yy][xx] = [x, y]
+                self._find_path(xx, yy)
 
+        if self.visited[y][x] is False:
+            self.visited[y][x] = True
+
+    def _get_minimum_path(self, sx, sy, ex, ey):
+        x, y = self.prev[ey][ex]
         while True:
-            if (x, y) == (ex, ey):
+            self._change_string(self.convert_maze, x, y, '.')
+            x, y = self.prev[y][x]
+            if (x, y) == (sx, sy):
                 break
-            if visited[y][x] is not None and distance[y][x] > distance[py][px] + 1:
-                direction = [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1)]
-                stack += direction
 
-                visited[y][x] = True
-                distance[y][x] = distance[py][px] + 1
-                prev[y][x] = [px, py]
-                px, py = x, y
-                self.change_maze(convert_maze, x, y, '.')
+    # 문자열을 변경해주는 함수. 코드 간결화하기 위해 사용
+    @staticmethod
+    def _change_string(string, x, y, to_string):
+        string[y] = string[y][:x] + to_string + string[y][x + 1:]
 
-            now = stack.pop()
-            x, y = now[0], now[1]
-
-        px, py = prev[ey][ex]
-        # while True:
-        #     prev[py][px] = x, y
-        #     self.change_maze(convert_maze, x, y, '.')
-
-    def change_maze(self, maze, x, y, to_string):
-        maze[y] = maze[y][:x] + to_string + maze[y][x + 1:]
-        print('y,x', maze[y][x], x, y)
-        for i in maze: print(i)
-
-    def copy_multidimensional_list(self, **kwargs):
+    @staticmethod
+    def _copy_multidimensional_list(**kwargs):
         multidimensional_list = []
         for i in range(len(kwargs['li'])):
             multidimensional_list.append([])
@@ -114,17 +139,19 @@ class Maze:
         return multidimensional_list
 
     # 출발 ,도착 지점의 index(좌표) 를 계산.(리스트로부터)
-    def get_start_end_point(self, convert_maze):
-        for y in range(len(convert_maze)):
-            for x, s in enumerate(convert_maze[y]):
+    def _get_start_end_point(self):
+        (sx, sy), (ex, ey) = (None, None), (None, None)
+        for y in range(len(self.convert_maze)):
+            for x, s in enumerate(self.convert_maze[y]):
                 if s == 'S':
                     sx, sy = x, y
                 elif s == 'E':
                     ex, ey = x, y
         return (sx, sy), (ex, ey)
 
-    # 출발, 도착 지점을 self.maze 에 만듭니다. random_point=True 라면 랜덤으로 지점 생성합니다.
-    def set_start_end(self, random_point=False):
+    # 출발, 도착 지점을 self._maze 에 만듭니다. random_point=True 라면 랜덤으로 지점 생성합니다.
+    def set_start_end_point(self, random_point=False):
+        start_point, end_point = None, None
         if random_point:
             while True:
                 start_point = randrange(len(self.maze))
@@ -144,8 +171,6 @@ class Maze:
         self.maze = self.maze[:start_point] + 'S' + self.maze[start_point + 1:]
         self.maze = self.maze[:end_point] + 'E' + self.maze[end_point + 1:]
 
-    # 문자열을 변경해주는 함수. 코드 간결화하기 위해 사용
-
     def __repr__(self):
         return f'Maze({self.w}, {self.h})'
 
@@ -154,12 +179,13 @@ class Maze:
 
 
 if __name__ == '__main__':
-    m1 = Maze(10, 5)
+    seed()
+    m1 = Maze(6, 3)
     print(repr(m1))
-
-    m1.set_start_end(
-        # random_point=True
-    )
     print(m1)
 
-    m1.solve()
+    # print(m1.solve())
+    print(m1.node_size)
+    m1.node_size = 1
+    print(m1.node_size)
+    # print(m1._node_size)
